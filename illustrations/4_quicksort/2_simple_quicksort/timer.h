6bf0e4a2f76cff59090071501451fc2b65dec8ec
Andy Curtis <contactandyc@gmail.com>
2019-08-30 15:45:07 -0700

    Updated timer to include an overall timing

---
+++ illustrations/4_quicksort/2_simple_quicksort/timer.h
@@ -0,0 +1,43 @@
+#ifndef _timer_H
+#define _timer_H
+
+struct timer_s;
+typedef struct timer_s timer_t;
+
+/*
+   Initialize the timer.  repeat is necessary to indicate how many times the
+   test was repeated so that the final result represents that.  If a thing is
+   only being timed with a single repetition, then use a value of 1.
+*/
+timer_t *timer_init(int repeat);
+
+/*
+   Initialize a timer from another timer.  This will subtract the time spent
+   from the other timer and set the repeat to match the other timer.
+*/
+timer_t *timer_timer_init(timer_t *t);
+
+/* destroy the timer */
+void timer_destroy(timer_t *t);
+
+/* Subtract the amount of time spent in sub from the current timer */
+void timer_subtract(timer_t *t, timer_t *sub);
+/* Add the amount of time spent in add to the current timer */
+void timer_add(timer_t *t, timer_t *add);
+
+/* start the timer */
+void timer_start(timer_t *t);
+
+/* end the timer */
+void timer_end(timer_t *t);
+
+/*
+  Returns time spent in nanoseconds (ns), microseconds (mis), milliseconds (ms),
+  and seconds (sec).
+*/
+double timer_ns(timer_t *t);
+double timer_mis(timer_t *t);
+double timer_ms(timer_t *t);
+double timer_sec(timer_t *t);
+
+#endif
