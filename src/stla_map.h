07046983cbd2117ada708a9d4fc6221225abe9b6
Andy Curtis <contactandyc@gmail.com>
2019-09-23 15:02:10 -0700

    Added first version of stla_map.h/c

---
+++ src/stla_map.h
@@ -0,0 +1,119 @@
+/*
+Copyright 2019 Andy Curtis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef _stla_map_H
+#define _stla_map_H
+
+/* still a bit incomplete */
+
+#include "stla_common.h"
+#include "stla_pool.h"
+#include "stla_buffer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stla_map_node_s;
+typedef struct stla_map_node_s stla_map_node_t;
+
+struct stla_map_node_s {
+  size_t parent_color;
+  stla_map_node_t *left;
+  stla_map_node_t *right;
+};
+
+/* iteration */
+stla_map_node_t * stla_map_first( stla_map_node_t *n );
+stla_map_node_t * stla_map_last( stla_map_node_t *n );
+stla_map_node_t * stla_map_next( stla_map_node_t *n );
+stla_map_node_t * stla_map_previous( stla_map_node_t *n );
+stla_map_node_t * stla_map_postorder_first( stla_map_node_t *n );
+stla_map_node_t * stla_map_postorder_next( stla_map_node_t *n );
+
+/*
+  stla_map_copy_node_f is a callback meant to be used with stla_map_copy to
+  copy one map to another given a root node.  The tag will typically be an
+  allocator such as the pool.
+*/
+typedef stla_map_node_t * (*stla_map_copy_node_f)( stla_map_node_t *n, void *tag );
+
+/*
+  print_node_to_string_f is a callback meant to print the value of the node n.
+  There is an expectation that the value will be printed on a single line.
+  Printing the internal representation of the tree is largely meant for testing
+  and doesn't need to be complete.
+*/
+typedef char * (*print_node_to_string_f)(stla_pool_t *pool, stla_map_node_t *n);
+
+
+stla_map_node_t * stla_map_copy( stla_map_node_t *root, stla_map_copy_node_f copy, void *tag);
+
+bool stla_map_valid(stla_buffer_t *bh, stla_pool_t *pool,
+                    stla_map_node_t *root, print_node_to_string_f print_node );
+
+#define STLA_MAP_DONT_PRINT_RED 1
+#define STLA_MAP_DONT_PRINT_BLACK_HEIGHT 2
+
+void stla_map_print_to_buffer(stla_buffer_t *bh,
+                              stla_pool_t *pool, stla_map_node_t *node,
+                              print_node_to_string_f print_node,
+                              int flags );
+
+void stla_map_fix_insert(stla_map_node_t *node,
+                         stla_map_node_t *parent,
+                         stla_map_node_t **root);
+
+bool stla_map_erase(stla_map_node_t *node, stla_map_node_t **root);
+
+/*
+  The stla_map_find and stla_map_insert methods are the only functions which
+  require access to the key/value members of the structure.  Because of this,
+  it is efficient to define these as macros.  stla_map_insert must call the
+  stla_map_fix_insert call to balance the tree after the node is inserted as a
+  leaf.
+*/
+
+stla_map_node_t *stla_map_node_find(stla_map_node_t *p, stla_map_node_t *root) {
+  while (root) {
+    if (p < root)
+      root = root->left;
+    else if(p > root)
+      root = root->right;
+    else
+      return root;
+  }
+  return NULL;
+}
+
+bool stla_map_node_insert(stla_map_node_t *node_to_insert, stla_map_node_t **root) {
+  stla_map_node_t **n = root, *parent = NULL;
+  while (*n) {
+    parent = *n;
+    /*
+    if (node_to_insert->key < parent->key)
+      n = &(parent->left);
+    else if (node_to_insert->key > parent->key)
+      n = &(parent->right);
+    else
+      return false;
+      */
+  }
+
+  *n = node_to_insert;
+  stla_map_fix_insert(node_to_insert, parent, root);
+  return true;
+}
+
+#endif
