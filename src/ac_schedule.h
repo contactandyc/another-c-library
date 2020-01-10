abb088be1003868d3af474edd29529ee1458bedf
Andy Curtis <contactandyc@gmail.com>
2020-01-10 09:58:35 -0800

    latest

--- src/ac_schedule.h
+++ src/ac_schedule.h
@@ -13,84 +13,43 @@ typedef struct ac_schedule_s ac_schedule_t;
 struct ac_task_s;
 typedef struct ac_task_s ac_task_t;
 
-struct ac_task_state_link_s;
-typedef struct ac_task_state_link_s ac_task_state_link_t;
-
 struct ac_task_part_s;
 typedef struct ac_task_part_s ac_task_part_t;
 
-struct ac_task_output_s;
-typedef struct ac_task_output_s ac_task_output_t;
-
 struct ac_task_input_s;
 typedef struct ac_task_input_s ac_task_input_t;
-
-struct ac_task_link_s;
-typedef struct ac_task_link_s ac_task_link_t;
-
-struct ac_task_link_s {
-  ac_task_t *task;
-  ac_task_link_t *next;
-};
 
 typedef bool (*ac_task_f)(ac_task_t *task);
 typedef bool (*ac_task_part_f)(ac_task_part_t *tp);
 typedef bool (*ac_task_check_input_f)(ac_task_part_t *tp, ac_task_input_t *inp,
                                       time_t ack_time);
 
-struct ac_task_part_s {
-  ac_pool_t *pool;
-  ac_buffer_t *bh;
-  ac_task_t *task;
-  size_t partition;
-  size_t num_partitions;
-  size_t running;
-  size_t thread_id;
-  time_t ack_time;
-  ac_task_state_link_t *__link;
-};
+/**************************************************************************
+The following functions should be called to setup the scheduler.  Once all
+of the tasks are scheduled, call ac_schedule_run.  This will cause the setup
+functions to be called in the order that they were scheduled.  The scheduler
+allows you to define a number of partitions, cpus, ram, and disk space.  It
+then attempts to keep your process operating fully within that space.
+***************************************************************************/
 
-struct ac_task_input_s {
-  char *name;
-  ac_task_check_input_f check;
-  double ram_pct;
-  ac_task_t *task;
-  ac_task_output_t *src;
-  ac_task_input_t *next;
-};
-
-struct ac_task_output_s {
-  char *name;
-  ac_task_link_t *destinations;
-  double ram_pct;
-  size_t flags;
-  size_t num_partitions;
-  ac_task_t *task;
-  ac_task_output_t *next;
-};
-
-ac_task_output_t *ac_task_get_output(ac_task_part_t *tp, size_t pos);
-ac_task_input_t *ac_task_get_input(ac_task_part_t *tp, size_t pos);
-
-const char *ac_task_name(ac_task_t *task);
-
-ac_schedule_t *ac_task_schedule(ac_task_t *task);
-
-size_t ac_task_part_ram(ac_task_part_t *tp, double pct);
-char *ac_task_output_base(ac_task_part_t *tp, ac_task_output_t *outp);
-char *ac_task_output_base2(ac_task_part_t *tp, ac_task_output_t *outp,
-                           const char *suffix);
-char *ac_task_input_name(ac_task_part_t *tp, ac_task_input_t *inp,
-                         size_t partition);
-
+/* Create the scheduler and specify how many partitions, cpus, ram (in MB),
+   and disk space.  Disk space is currently unused - but I have plans for it. */
 ac_schedule_t *ac_schedule_init(size_t num_partitions, size_t cpus, size_t ram,
                                 size_t disk_space);
+
+/* Define where the ack directory should be (default is tasks/ack)*/
 void ac_schedule_ack_dir(ac_schedule_t *h, const char *ack_dir);
+
+/* Define where the tasks directory should be output to (default is tasks) */
 void ac_schedule_task_dir(ac_schedule_t *h, const char *task_dir);
 
+/* Add a task to the scheduler with an associated name and define whether it
+   is partitioned or not.  The setup function will further describe the task
+   once all of the tasks have been added to the scheduler. */
 ac_task_t *ac_schedule_task(ac_schedule_t *h, const char *task_name,
                             bool partitioned, ac_task_f setup);
 
+/* Run all of the tasks */
 void ac_schedule_run(ac_schedule_t *h, ac_task_part_f on_complete);
 
 /* Destroy the scheduler */
@@ -155,6 +114,89 @@ void ac_task_do_nothing(ac_task_t *task);
 
 /* this forces a task to run everytime no matter what */
 void ac_task_run_everytime(ac_task_t *task);
+
+/**************************************************************************
+The following structures are primarily used within the runner
+***************************************************************************/
+
+struct ac_task_state_link_s;
+typedef struct ac_task_state_link_s ac_task_state_link_t;
+
+struct ac_task_output_s;
+typedef struct ac_task_output_s ac_task_output_t;
+
+struct ac_task_link_s;
+typedef struct ac_task_link_s ac_task_link_t;
+
+struct ac_task_link_s {
+  ac_task_t *task;
+  ac_task_link_t *next;
+};
+
+struct ac_task_part_s {
+  ac_pool_t *pool;
+  ac_buffer_t *bh;
+  ac_task_t *task;
+  size_t partition;
+  size_t num_partitions;
+  size_t running;
+  size_t thread_id;
+  time_t ack_time;
+  ac_task_state_link_t *__link;
+};
+
+struct ac_task_input_s {
+  char *name;
+  ac_task_check_input_f check;
+  double ram_pct;
+  ac_task_t *task;
+  ac_task_output_t *src;
+  ac_task_input_t *next;
+};
+
+struct ac_task_output_s {
+  char *name;
+  ac_task_link_t *destinations;
+  double ram_pct;
+  size_t flags;
+  size_t num_partitions;
+  ac_task_t *task;
+  ac_task_output_t *next;
+};
+
+/**************************************************************************
+The following functions are meant to be used within the runner or the check
+callback.
+***************************************************************************/
+
+/* Get nth output (defined in the setup callback) */
+ac_task_output_t *ac_task_get_output(ac_task_part_t *tp, size_t n);
+
+/* Get nth input (defined by ac_task_input_files and by ac_task_output) */
+ac_task_input_t *ac_task_get_input(ac_task_part_t *tp, size_t n);
+
+/* Return the task name given a task */
+const char *ac_task_name(ac_task_t *task);
+
+/* Get the scheduler given a task - shouldn't be needed much if at all */
+ac_schedule_t *ac_task_schedule(ac_task_t *task);
+
+/* Return an actual amount of ram given the task/partition and percentage */
+size_t ac_task_part_ram(ac_task_part_t *tp, double pct);
+
+/* Return the output base name based upon a task/partition and an output */
+char *ac_task_output_base(ac_task_part_t *tp, ac_task_output_t *outp);
+
+/* Same as above except adding a suffix.  Suffix is somewhat complicated in
+   the case where filenames have a compression extension. */
+char *ac_task_output_base2(ac_task_part_t *tp, ac_task_output_t *outp,
+                           const char *suffix);
+
+/* Returns NULL if no input from the given source partition.  Otherwise,
+   this will return the name of the file that was output to this partition
+   given the task/part, input, and source partition */
+char *ac_task_input_name(ac_task_part_t *tp, ac_task_input_t *inp,
+                         size_t partition);
 
 /**************************************************************************
 Other helper functions
