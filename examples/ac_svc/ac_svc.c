c99f5cda1718fb55107bbe24ccd6d2dbb42a91d1
Andy Curtis <contactandyc@gmail.com>
2020-02-19 09:23:52 -0800

    Updated

--- examples/ac_svc/ac_svc.c
+++ examples/ac_svc/ac_svc.c
@@ -1,7 +1,14 @@
 #include "ac_svc.h"
 
+int on_url(ac_svc_request_t *h) {
+  static char hello_world_s[] = "Hello Service\n";
+  h->output.base = (char *)hello_world_s;
+  h->output.len = sizeof(hello_world_s) - 1;
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-  ac_svc_t *s = ac_svc_port_init(7000, NULL, NULL);
+  ac_svc_t *s = ac_svc_port_init(7000, on_url, NULL);
   ac_svc_threads(s, 10);
   ac_svc_backlog(s, 10000);
   ac_svc_run(s);
