5664c813be458f8d55a7f064ab8ecf71f460c038
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-11 16:35:34 -0400

    Updated footer

--- website/src/components/footer.js
+++ website/src/components/footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 function Footer() {
 	return (
 		<footer className="Footer">
-			© {new Date().getFullYear()}. Built with{' '}
-			<a className="Footer-a" href="https://www.gatsbyjs.org">
-				Gatsby
-			</a>{' '}
-			by{' '}
-			<a className="Footer-a" href="https://www.curtiscodes.com">
-				Daniel Curtis
+			© {new Date().getFullYear()} Andy Curtis & Daniel Curtis.{' '}
+			<Link to="/" className="Footer-a">
+				Home
+			</Link>{' '}
+			|{' '}
+			<Link to="/contact" className="Footer-a">
+				Contact
+			</Link>{' '}
+			|{' '}
+			<a
+				className="Footer-a"
+				href="https://www.github.com/contactandyc/another-c-library">
+				GitHub
 			</a>
-			.
 		</footer>
 	);
 }
