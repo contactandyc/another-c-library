9206ef91a8c4393105d7b180c8b2021e2ad72c5a
Andy Curtis <contactandyc@gmail.com>
2022-01-29 21:00:48 -0500

    updated

--- games/minesweeper/minesweeper.c
+++ games/minesweeper/minesweeper.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 void clear_screen() {
     printf( "\e[1;1H\e[2J" );
 }
 
+#define RED_START "\x1B[31m"
+#define RED_END "\x1B[0m"
+
 void print_token(int **board, int size, int i, int j ) {
-    if(board[i][j] == -1) {
+    if(board[i][j] == 0) {
         printf(" ? ");
+    }
+    else if(board[i][j] == -1) {
+        printf(RED_START " * " RED_END );
     }
 }
 
@@ -26,16 +34,29 @@ void print_minesweeper(int **board, int size) {
   }  
 }
 
+void place_bombs( int **board, int size, int num_bombs ) {
+    for( int i=0; i<num_bombs; i++ ) {
+        int x,y;
+        do {
+          x = rand() % size;
+          y = rand() % size;
+        } while(board[x][y] != 0);
+        board[x][y] = -1;
+    }
+}
+
 int main( int argc, char *argv[] ) {
+    srand(time(NULL));
     clear_screen();
     int size = 10;
     int **board = (int **)malloc(sizeof(int *)*size);
     for( int i=0; i<size; i++ ) {
         board[i] = (int *)malloc(sizeof(int)*size);
         for( int j=0; j<size; j++ ) {
-            board[i][j] = -1;
+            board[i][j] = 0;
         }
     }
+    place_bombs(board, size, 5);
     print_minesweeper(board, size);
     return 0;
 }
