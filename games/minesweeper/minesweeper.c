3388a1c5aedb46fcd146a3728065cc54c7ebd38d
Andy Curtis <contactandyc@gmail.com>
2022-01-29 21:36:44 -0500

    latest

--- games/minesweeper/minesweeper.c
+++ games/minesweeper/minesweeper.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 void clear_screen() {
     printf( "\e[1;1H\e[2J" );
@@ -22,6 +23,9 @@ void print_token(int **board, int size, int i, int j ) {
     }
     else if(board[i][j] == -1) {
         printf(RED_START " * " RED_END );
+    }
+    else if(board[i][j] > 0) {
+        printf(" %d ", board[i][j] - 1);
     }
 }
 
@@ -51,14 +55,68 @@ void place_bombs( int **board, int size, int num_bombs ) {
     }
 }
 
-void get_input() {
-    printf( "Your Move: " );
+typedef enum {INVALID_MOVE, GAME_OVER, GOOD_MOVE} move_t;
 
+int check_for_bomb(int **board, int size, int x, int y) {
+    if(x < 0 || x >= size) return 0;
+    if(y < 0 || y >= size) return 0;
+    if(board[x][y] == -1) return 1;
+    return 0;
 }
+
+move_t try_move(int **board, int size, int x, int y ) {
+    if(board[x][y] < 0)
+        return GAME_OVER;
+    else if(board[x][y] != 0)
+        return INVALID_MOVE;
+    else {
+        // how many bombs are around this?
+        int num_bombs = 
+            check_for_bomb(board, size, x-1, y-1) +
+            check_for_bomb(board, size, x, y-1 ) +
+            check_for_bomb(board, size, x+1, y-1) + 
+            check_for_bomb(board, size, x-1, y) +
+            check_for_bomb(board, size, x+1, y) + 
+            check_for_bomb(board, size, x-1, y+1) +
+            check_for_bomb(board, size, x, y+1 ) +
+            check_for_bomb(board, size, x+1, y+1); 
+        board[x][y] = num_bombs+1;
+        return GOOD_MOVE;
+    }
+}
+
+move_t do_move(int **board, int size, char *move) {
+    int move_result = INVALID_MOVE;
+    if(move[0] >= 'A' && move[0] < 'A' + size && 
+       move[1] >= '0' && move[1] < '0' + size) {
+      move_result = try_move(board, size,  move[1]-'0', move[0]-'A');
+    }
+    return move_result;
+}
+
+bool get_input(int **board, int size, char *move) {
+    printf( "\n   Your Move: " );
+    if(!fgets(move, 5, stdin))
+        return false;
+
+    if(move[0] >= 'a' && move[0] <='z')
+        move[0] = move[0] - 'a' + 'A';
+    return true;
+}
+
+bool did_i_win(int **board, int size) {
+    for( int i=0; i<size; i++ ) {
+        for( int j=0; j<size; j++ ) {
+            if(board[i][j] == 0)
+                return false;
+        }
+    }
+    return true;
+}
+
 
 int main( int argc, char *argv[] ) {
     srand(time(NULL));
-    clear_screen();
     int size = 10;
     int **board = (int **)malloc(sizeof(int *)*size);
     for( int i=0; i<size; i++ ) {
@@ -68,6 +126,28 @@ int main( int argc, char *argv[] ) {
         }
     }
     place_bombs(board, size, 5);
+    clear_screen();
     print_minesweeper(board, size);
+    char move[10];
+    while(true) {
+        if(!get_input(board, size, move))
+            break;
+        clear_screen();
+        move_t m = do_move(board, size, move);
+        print_minesweeper(board, size);
+        if(m == INVALID_MOVE)
+            printf( "%s is an invalid move!\n", move );
+        else if(m == GAME_OVER) {
+            printf( "There was a bomb at %s -- GAME OVER!\n", move);
+            break;
+        }
+        else if(m == GOOD_MOVE) {
+            if(did_i_win(board, size)) {
+                printf( "You Won!\n");
+                break;
+            }
+        }
+    }
+    printf( "Bye!\n");
     return 0;
 }
