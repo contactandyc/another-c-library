162d1cb8590365fa5b0f120cdf11f010af2f5c1c
Andy Curtis <ac@MacBook-Pro.local>
2023-08-16 20:27:05 -0400

    Moving files around

--- auto/include/ac-core/ac_sort/check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-diff ac_sort_m.h ac_sort_compare_m.h
-diff ac_sort_arg_m.h ac_sort_compare_arg_m.h
-sed "s/, arg//g" ac_sort_compare_arg_m.h > x.h
-diff -b x.h ac_sort_m.h
-rm x.h
--- auto/lib/ac-io/md5/README.md
+++
@@ -1,1 +0,0 @@
-The md5 package is taken from https://people.csail.mit.edu/rivest/Md5.c.  The original code exists in original/Md5.c.  It has been modified to meet the needs of the ac library.
