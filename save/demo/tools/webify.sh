d5f2a29a74c10f3dfd285874bf0f6080970bd5f0
Andy Curtis <contactandyc@gmail.com>
2023-10-01 19:37:27 -0400

    Updating library to a new style

--- save/demo/tools/webify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed 's/  /\&nbsp; /g' | sed 's/  /\&nbsp; /g' | sed 's/"/\&quot;/g' | sed 's/{/\&#123;/g' | sed 's/{/\&#125;/g' | sed 's/\</\&lt;/g' | sed 's/\>/\&gt;/g' | sed 's#$#<br\/>#g'
